0bb9f10aa9b433c1b71d2836fe9f21870cc64011
gian <nnkogift@hotmailcom>
2022-06-04 12:43:55 +0300

    :sparkles: feat(disaggregation form modal): Implementation
    - Implemented form modal

---
+++ src/modules/TemplateProgramIndicator/components/DisaggregationForm/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button, ButtonStrip, Modal, ModalActions, ModalContent, ModalTitle} from '@dhis2/ui'
+import i18n from '@dhis2/d2-i18n';
+
+
+export default function DisaggregationForm({
+                                               open,
+                                               onClose
+                                           }: { open: boolean, onClose: () => void }): React.ReactElement {
+
+    return (
+        <Modal position="middle" open={open} onClose={onClose}>
+            <ModalTitle>
+                {i18n.t("Create new disaggregation")}
+            </ModalTitle>
+            <ModalContent>
+                <p>{i18n.t("Disaggregation form")}</p>
+            </ModalContent>
+            <ModalActions>
+                <ButtonStrip>
+                    <Button onClick={onClose}>{i18n.t("Cancel")}</Button>
+                    <Button primary>{i18n.t("Save")}</Button>
+                </ButtonStrip>
+            </ModalActions>
+        </Modal>
+    )
+
+}
--- src/modules/TemplateProgramIndicator/index.tsx
+++ src/modules/TemplateProgramIndicator/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useParams} from "react-router-dom";
+import {Button} from '@dhis2/ui'
+import DisaggregationForm from "./components/DisaggregationForm";
+import i18n from '@dhis2/d2-i18n'
 
 
 export default function TemplateProgramIndicator(): React.ReactElement {
     const {id} = useParams<{ id: string }>();
+    const [open, setOpen] = useState(false);
 
     return (
-        <h1>Template indicator: {id}</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-12">
+                    <h1>Template indicator: {id}</h1>
+                    <Button onClick={() => setOpen(prevState => !prevState)}>{i18n.t("Open Disaggregator")}</Button>
+                </div>
+            </div>
+            {
+                open && (
+                    <DisaggregationForm open={open} onClose={() => setOpen(false)}/>
+                )
+            }
+        </div>
     )
 }
